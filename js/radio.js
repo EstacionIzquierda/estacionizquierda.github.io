21c28093ff06add963cadb4625b51d034e9b6fff
scio_station <114015214+sciostation@users.noreply.github.com>
2023-11-17 18:18:46 -0500

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     'use strict';
 
-    var liveRadioListUrl = "https://gist.githubusercontent.com/valarpirai/473305f09f8433f1d338634ed42c437d/raw/live-radio.json?id=" + new Date().getTime();;
+    var liveRadioListUrl = "https://gist.githubusercontent.com/softwaretro/9e9487af34e664ee0ba5ef714b2d2e9a/raw/cd9758840e5edf77b4d6170f50c166f0fe7f9a5d/live-radio.json?id=" + new Date().getTime();;
 
     var stationList = {};
     var selectedStationList = [];
